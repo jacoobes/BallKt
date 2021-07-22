c50b3e30eecf160ab3201a51a457a9d27eed0cb4
Cannon <teamnapinfo@gmail.com>
2021-07-22 17:05:07 +0100

    added request again. but better

--- typescript/managers/Manager.ts
+++ typescript/managers/Manager.ts
@@ -1,6 +1,10 @@
 import { Collection } from "@discordjs/collection"
-
+import fetch from "node-fetch"
 export default class Manager {
     protected readonly endpoint = "https://www.balldontlie.io/api/v1/"
     public cache = new Collection()
+
+    async request(endpoint: string) {
+        return (await fetch(endpoint)).json()
+    }
 }
