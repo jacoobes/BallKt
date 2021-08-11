257cabdde00d75a9f7fd08c6b1bc5b0383e6c6a3
Cannon <73777697+CannonStealth@users.noreply.github.com>
2021-08-11 01:29:11 +0100

    Update TeamManager.ts

--- typescript/managers/TeamManager.ts
+++ typescript/managers/TeamManager.ts
@@ -1,12 +1,20 @@
 import Manager from "./Manager";
 import Team, { APITeam } from "../classes/Team";
-import { numbers as range1_100, NoTypes } from "../utils";
+import { NoTypes } from "../utils";
 
 /**
  * @class
  */
  export default class TeamManager extends Manager<Team["id"], Team> {
-     async fetch() {
-         
+     async fetch<T extends all | id>(type: T, options: T extends id ? number : { page?: number, amount?: number }) {
+         if ([0, "all", "every"].includes(type)) {
+
+             const { data } = await this.request("teams");
+
+
+         }
      }
  }
+
+type all = 0 | "all" | "every"
+type id = 1 | "id"
