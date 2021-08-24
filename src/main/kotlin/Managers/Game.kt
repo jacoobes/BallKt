2890f3358ec4fc5e25a8cbe57334dbcfaff7a390
jacoobes <jacobnguyend@gmail.com>
2021-08-23 23:55:44 -0500

    Serializers for GameData.kt

--- src/main/kotlin/Managers/Game.kt
+++ src/main/kotlin/Managers/Game.kt
@@ -1,5 +1,6 @@
 package dev.seren.Managers
 
+import com.github.kittinunf.fuel.httpGet
 import dev.seren.BallCache
 import dev.seren.serializables.game.GameData
 import dev.seren.serializables.team.TeamData
@@ -11,4 +12,10 @@ class Game : BallManager() {
 
     internal val cache = BallCache<Int, GameData>(60)
 
+    fun fetchById(id: Int) {
+        if(cache hasKey id) cache[id]
+        "${basePath}/games/$id"
+            .httpGet()
+
+    }
 }
